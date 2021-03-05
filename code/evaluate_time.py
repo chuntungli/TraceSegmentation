162d14c560944542ca37798956c59b9d31e123c5
dan <chun-tung.li@connect.polyu.hk>
2021-03-05 15:22:41 +0800

    Minor Update

--- code/evaluate_time.py
+++ code/evaluate_time.py
@@ -60,14 +60,11 @@ def safe_div(a,b):
 def evaluate(data_folder, gt_folder):
     performance_record = []
     time_record = []
-    should_skip = False
+    skip_gb = skip_spam = skip_vmsp = False
 
     for value in sorted(os.listdir(data_folder)):
         if not value.isdigit():
             continue
-
-        if should_skip:
-            break
 
         folds = sorted(os.listdir('%s/%s' % (data_folder, value)))
 
@@ -91,99 +88,6 @@ def evaluate(data_folder, gt_folder):
             print('\nRuntime of TRASE: %.2fs\tNo. of Patterns: %d' % (TRASE_time, len(Z)))
             time_record.append(('TRASE', int(value), fold, '%.3f' % TRASE_time))
 
-            # for i in range(len(Z) - 1, 0, -1):
-            #     for j in range(len(Z)):
-            #         if i == j:
-            #             continue
-            #         # Z[i] and Z[j] have the same support and Z[i] is a subsequence of Z[j]
-            #         if (Z[i].support == Z[j].support) & (is_subsequence(Z[i].pattern, Z[j].pattern)):
-            #             # print('Z[%d] is a subsequence of Z[%d]: (%s) and (%s)' % (i, j, Z[i].pattern, Z[j].pattern))
-            #             # Delete Z[i]
-            #             del Z[i]
-            #             break
-            #
-            # Z = np.array(sorted(Z, key=lambda pattern: pattern.support, reverse=True))
-            #
-            # # Vertex list contains the weight of the phase
-            # # Edge list contains relationship among phases if two phases are overlapped
-            # vertex_list = []
-            # edge_list = []
-            # for i in range(len(Z)):
-            #     # Weight is defined as the number_of_method * support_of_phase
-            #     vertex_list.append(Z[i].support * sum([len(id_list.ids[x]) for x in Z[i].pattern]))
-            #     for j in range(i + 1, len(Z)):
-            #         if is_intersect(Z[i], Z[j]):
-            #             # print('%d and %d are overlapped' % (i, j))
-            #             edge_list.append((i, j))
-            #             edge_list.append((j, i))
-            # vertex_list = np.array(vertex_list)
-            # edge_list = np.array(edge_list)
-            #
-            # adjacency_list = [[] for __ in vertex_list]
-            # for edge in edge_list:
-            #     adjacency_list[edge[0]].append(edge[1])
-            # adjacency_list = np.array(adjacency_list)
-            #
-            # subgraphs = generateSubgraphs(vertex_list, adjacency_list)
-            #
-            # solution = np.zeros(len(vertex_list), dtype=bool)
-            # for subgraph in subgraphs:
-            #     vl = np.array(copy.deepcopy(vertex_list[subgraph]))
-            #     al = np.array(copy.deepcopy(adjacency_list[subgraph]))
-            #     for i in range(len(al)):
-            #         for j in range(len(al[i])):
-            #             al[i][j] = np.where(subgraph == al[i][j])[0][0]
-            #     OPT_X = MWIS(vl, al)
-            #     solution[subgraph] = OPT_X
-            #
-            # patterns = Z[solution]
-            #
-            # # Print Pattern Result
-            # # for pattern in patterns:
-            # #     print('%.2f\t%s' % (pattern.support, pattern.pattern))
-            #
-            # # Construct list of groundtruth
-            # labels = list(groundtruth.keys())
-            #
-            # gt_dict = dict()
-            # pt_dict = dict()
-            # pt_count = dict()
-            #
-            # # Construct gt_dict
-            # for label in labels:
-            #     gt_dict[label] = set().union(*groundtruth[label]['pattern'])
-            #     pt_dict[label] = set()
-            #     pt_count[label] = 0
-            #
-            # for pattern in patterns:
-            #     ml_pattern = []
-            #     for pid in pattern.pattern:
-            #         ml_pattern.append(id_list.ids[pid])
-            #     ml_pattern = set().union(*ml_pattern)
-            #
-            #     # Find the best matching groundtruth
-            #     accuracy = dict()
-            #     for label in labels:
-            #         accuracy[label] = len(gt_dict[label].intersection(ml_pattern)) / len(ml_pattern)
-            #
-            #     prediction = max(accuracy, key=accuracy.get)
-            #     pt_dict[prediction] = pt_dict[prediction].union(ml_pattern)
-            #     pt_count[prediction] += 1
-            #
-            # # Construct Confusion Matrix
-            # for label in labels:
-            #     pt_set = pt_dict[label]
-            #     gt_set = gt_dict[label]
-            #
-            #     TP = len(gt_set.intersection(pt_set))
-            #     FP = len(pt_set.difference(gt_set))
-            #     FN = len(gt_set.difference(pt_set))
-            #
-            #     precision = safe_div(TP, (TP + FP))
-            #     recall = safe_div(TP, (TP + FN))
-            #
-            #     performance_record.append((int(value), fold, label, pt_count[label], precision, recall))
-
             '''
                 =================================================================
                                     Gap-Bide Algorithm
@@ -198,49 +102,113 @@ def evaluate(data_folder, gt_folder):
                     s.append(id_list.ids.index(event))
                 sdb.append(s)
 
+            if not skip_gb:
+                start = time.time()
+
+                gb = Gapbide(sdb, int(min_sup * id_list.n_traces), 0, max_gap - 1)
+
+                q = mp.Queue()
+                p = mp.Process(target=gb.run, args=(q,))
+                p.start()
+
+                try:
+                    patterns = q.get(timeout=time_out)
+                except Exception:
+                    patterns = []
+
+                p.join(timeout=time_out)
+
+                if p.is_alive():
+                    p.terminate()
+                    GB_time = np.inf
+                    skip_gb = True
+                    print('GAP-BIDE time out at value: %s' % value)
+                    break
+                else:
+                    GB_time = time.time() - start
+
+                print('\nRuntime of Gap-Bide: %.2fs\tNo. of Patterns: %d' % (GB_time, len(patterns)))
+                time_record.append(('GAP-BIDE', int(value), fold, '%.3f' % GB_time))
+
+            '''
+                =================================================================
+                                        VMSP Algorithm
+                =================================================================
+            '''
+
             # Write data to file
-            # f = open("%s.txt" % app, "w+")
-            # for trace in temp_data:
-            #     for i in range(len(trace)):
-            #         f.write('%d -1 ' % trace[i])
-            #     f.write('-2\r\n')
-            # f.close()
+            f = open("%s.txt" % app, "w+")
+            for trace in temp_data:
+                for i in range(len(trace)):
+                    f.write('%d -1 ' % trace[i])
+                f.write('-2\r\n')
+            f.close()
 
-            # All Sequential Patterns
-            # spmf = Spmf("SPAM", input_filename="%s.txt" % app, output_filename="output.txt", arguments=[0.6, 5, 500, max_gap, False])
+            if not skip_vmsp:
 
-            # Closed Sequential Patterns
-            start = time.time()
+                start = time.time()
 
-            gb = Gapbide(sdb, int(min_sup * id_list.n_traces), 0, max_gap - 1)
+                # Maximal Sequential Patterns
+                spmf = Spmf("VMSP", input_filename="%s.txt" % app, output_filename="output.txt",
+                            arguments=['%d%%' % (min_support * 100), 100, max_gap, False])
 
-            q = mp.Queue()
-            p = mp.Process(target=gb.run, args=(q,))
-            p.start()
+                p = mp.Process(target=spmf.run)
+                p.start()
 
-            try:
-                patterns = q.get(timeout=time_out)
-            except Exception:
-                patterns = []
+                p.join(timeout=time_out)
 
-            p.join(timeout=time_out)
+                if p.is_alive():
+                    p.terminate()
+                    vmsp_time = np.inf
+                    skip_vmsp = True
+                    print('VMSP time out at value: %s' % value)
+                    break
+                else:
+                    vmsp_time = time.time() - start
 
-            if p.is_alive():
-                p.terminate()
-                GB_time = np.inf
-                should_skip = True
-                print('Method time out at value: %s' % value)
-                break
-            else:
-                GB_time = time.time() - start
+                print('\nRuntime of VMSP: %.2fs\tNo. of Patterns: %d' % (vmsp_time, len(spmf.to_pandas_dataframe(pickle=True))))
+                time_record.append(('VMSP', int(value), fold, '%.3f' % vmsp_time))
 
-            print('\nRuntime of Gap-Bide: %.2fs\tNo. of Patterns: %d' % (GB_time, len(patterns)))
-            time_record.append(('GAP-BIDE', int(value), fold, '%.3f' % GB_time))
+            '''
+                =================================================================
+                                        SPAM Algorithm
+                =================================================================
+            '''
 
-            # # Maximal Sequential Patterns
-            # spmf = Spmf("VMSP", input_filename="%s.txt" % app, output_filename="output.txt", arguments=['%d%%' % (min_support * 100), 500, max_gap, False])
-            # spmf.run()
-            # print(spmf.to_pandas_dataframe(pickle=True))
+            # Write data to file
+            f = open("%s.txt" % app, "w+")
+            for trace in temp_data:
+                for i in range(len(trace)):
+                    f.write('%d -1 ' % trace[i])
+                f.write('-2\r\n')
+            f.close()
+
+            if not skip_spam:
+
+                start = time.time()
+
+                # Maximal Sequential Patterns
+                spmf = Spmf("SPAM", input_filename="%s.txt" % app, output_filename="output.txt",
+                            arguments=['%d%%' % (min_support * 100), 10, 100, max_gap, False])
+
+                p = mp.Process(target=spmf.run)
+                p.start()
+
+                p.join(timeout=time_out)
+
+                if p.is_alive():
+                    p.terminate()
+                    spam_time = np.inf
+                    skip_spam = True
+                    print('SPAM time out at value: %s' % value)
+                    break
+                else:
+                    spam_time = time.time() - start
+
+                print('\nRuntime of SPAM: %.2fs\tNo. of Patterns: %d' % (
+                spam_time, len(spmf.to_pandas_dataframe(pickle=True))))
+                time_record.append(('SPAM', int(value), fold, '%.3f' % spam_time))
+
 
     return (time_record, performance_record)
 
